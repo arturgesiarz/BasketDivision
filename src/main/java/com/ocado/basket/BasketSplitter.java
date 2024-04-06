e25d41ddd04b05a30ef77f9b4d786144ffb9b9b9
Artur Gęsiarz <arturgesiarz@student.agh.edu.pl>
2024-04-06 11:48:27 +0200

    Added setDelivareOptions and added get for deliveryOptonsForProducts

--- src/main/java/com/ocado/basket/BasketSplitter.java
+++ src/main/java/com/ocado/basket/BasketSplitter.java
@@ -1,12 +1,50 @@
 package com.ocado.basket;
-import java.util.List;
-import java.util.Map;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class BasketSplitter {
+    private final Map<String, List<String>> deliveryOptionsForProducts = new HashMap<>();
 
     public BasketSplitter(String absolutePathToConfigFile) {
+        setDeliveryOptions(absolutePathToConfigFile);
     }
+
+    private void setDeliveryOptions(String absolutePathToConfigFile) {
+        JSONParser parser = new JSONParser();
+
+        try (FileReader reader = new FileReader(absolutePathToConfigFile)){
+
+            Object obj = parser.parse(reader);
+            JSONObject jsonObject = (JSONObject) obj;
+
+            for (Object key : jsonObject.keySet()) {
+                String product = (String) key;
+                List<String> deliveryOption = (List<String>) jsonObject.get(key);
+                deliveryOptionsForProducts.put(product, deliveryOption);
+            }
+
+        } catch (IOException | ParseException  e) {
+            e.printStackTrace();
+        }
+    }
+
     public Map<String, List<String>> split(List<String> items) {
         return null;
+    }
+
+    public Map<String, List<String>> getDeliveryOptionsForProducts() {
+        return Collections.unmodifiableMap(deliveryOptionsForProducts);
+    }
+
+    public static void main(String[] args) {
+        String absolutPathTOConfigFile = "/Users/arturgesiarz/IdeaProjects/Java/BasketDivision/src/main/resources/config.json";
+        BasketSplitter basketSplitter = new BasketSplitter(absolutPathTOConfigFile);
+        Map<String, List<String>> deliveryOptionTest = basketSplitter.getDeliveryOptionsForProducts();
+        deliveryOptionTest.get("Bread - Crumbs, Bulk").forEach(System.out::println);
     }
 }
